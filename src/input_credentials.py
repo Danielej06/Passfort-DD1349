437838c43f0cd2f8de3e90778191fc610d249c53
Daniel <daeo@kth.se>
2026-04-28 20:15:33 +0200

    inputcredentials doesn't overwrite anymore

--- src/input_credentials.py
+++ src/input_credentials.py
@@ -1,5 +1,6 @@
 ## This file is used to input the credentials and save them to a json file.
 def input_credentials():
+    import os
     import json
     
     print("Enter the name of the credentials")
@@ -10,12 +11,24 @@ def input_credentials():
     password = input()
     print("Enter the url for", title)
     url = input()
-    credentials = {
-        "title": title,
-        "username": username,
-        "password": password,
-        "url": url
-    }
+    # Check if credentials file is there
+    if os.path.isfile("data.json") == False:
+        credentials = {
+            title: {
+                "username": username,
+                "password": password,
+                "url": url
+            }
+        }
+    else:
+        with open('data.json', 'r') as saved_credentials:
+            credentials = json.load(saved_credentials)
+        credentials[title] = {
+                "username": username,
+                "password": password,
+                "url": url
+        }
+    ##open data.json to write and save credentials to the file
     with open('data.json', 'w') as saved_credentials:
         json.dump(credentials, saved_credentials)
     
