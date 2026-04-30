36a7bba8cdeca2f213834e0af58bcb20e69debd2
Tim Blomqvist <timblomqvist@MacBook-Air-som-tillhor-Tim.local>
2026-04-30 11:49:16 +0200

    Ny fil + minimala variabel ändringar i kdf

---
+++ src/crypto/aes.py
@@ -0,0 +1,40 @@
+# Struktur innan jag börjar skriva
+# Vi har 32 byte hash från kdf
+# Denna fil ska ta ett normalt lösenord, enkryptera med kdf hashet, och ge vår ciphertext + nonce
+# Dessutom ska denna fil ha inversen till denna funktion tillgänglig.
+
+import secrets
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM # Fernet är supposedly mycket enklare.
+# Den ska tydligen kunna göra allt detta direkt, men då lär jag mig inget så försöker med denna istället.
+
+# Genererar ett värde som endast används en gång. Matchas med lösenordet 
+def generate_nonce() -> bytes:
+
+    return secrets.token_bytes(12)    
+
+
+def encrypt(data: bytes, key: bytes) -> dict:
+    nonce = generate_nonce()
+    
+    aes = AESGCM(key)
+    ciphertext = aes.encrypt(nonce, data, None)
+    encrypted_data = {
+        "ciphertext": ciphertext,
+        "nonce": nonce
+    }
+
+    return encrypted_data
+    
+
+
+def decrypt(payload: dict, key: bytes) -> bytes:
+
+
+    aes = AESGCM(key)
+    ciphertext = payload["ciphertext"]
+    nonce = payload["nonce"]
+    
+    plaintext = aes.decrypt(nonce, ciphertext, None)
+
+    return plaintext
+
--- src/crypto/kdf.py
+++ src/crypto/kdf.py
@@ -2,6 +2,9 @@ import secrets
 from argon2.low_level import hash_secret_raw, Type
 
 
+# Denna fil är "Key derivation feature." Innehåller funktioner för att derivera master password
+# till en hash, som senare används med AES.
+
 # Feel free att ändra dessa värden. La in dessa som initial eftersom de supposedly är balanced.
 # Hackers ska tycka det är jobbigt, men en user ska inte spendera 20 minuter för att hämta lösenord.
 salt_length = 16
@@ -18,10 +21,10 @@ def generate_salt() -> bytes:
 # Konverterar lösenordet till bytes. Detta används sen för att skapa vår key, inom hash_secret_raw.
 def derive_key(password: str, salt: bytes) -> bytes:
 
-    passwordBytes = password.encode("utf-8")
+    master_Password_Bytes = password.encode("utf-8")
 
     key = hash_secret_raw(
-    secret = passwordBytes, 
+    secret = master_Password_Bytes, 
     salt = salt,
     time_cost = time_cost,
     memory_cost = memory_cost,
@@ -33,5 +36,5 @@ def derive_key(password: str, salt: bytes) -> bytes:
 
 def create_key_from_password (password: str) -> tuple[bytes, bytes]:
     salt = generateSalt()
-    key = deriveKey(password, salt)
+    key = deriveKey(master_Password, salt)
     return key, salt
