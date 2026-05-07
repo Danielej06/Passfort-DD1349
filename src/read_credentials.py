179db9f85ebe7a0c2ff571283f747b37f4b6adf5
Danielej06 <ejike.dani1@gmail.com>
2026-05-07 20:15:32 +0200

    Feat/add encryption to develop Fixes #5 (#25)
    * Added the encryption to the product Fixes #5
    
    * Not allowing to read credentials while vault is empty
    
    * Fixed Akanksh' incompetence (doesnt crash on unsupported input)
    
    ---------
    
    Co-authored-by: Daniel <daeo@kth.se>

--- src/read_credentials.py
+++ src/read_credentials.py
@@ -1,11 +1,13 @@
-## This file is used to read one credential from the data.json file and print to the console.
+## This file is used to read one credential from the vault and print to the console aswell as changing the password.
+from crypto.vault import load_vault, save_vault
 from loading import loading
 
-def read_credentials():
-    import json
-    ##Open data.json and read to saved_credentials
-    with open('data.json', 'r') as saved_credentials:
-        credentials = json.load(saved_credentials)
+def read_credentials(master_password):
+    ##Open the vault and read the contents to saved_credentials
+    if load_vault(master_password) == {} : ## ISSUE: The "null" situation just crashes the code, needs to be fixed.
+        print("Sorry there are no credentials saved yet. Returning to main menu.")
+        return
+    credentials = load_vault(master_password)
 
     i = 1
     print("Here are your saved credentials:")
@@ -13,21 +15,23 @@ def read_credentials():
         print(i, ". " , cr_name)
         i += 1
     
-    choice = input("Which one would you like to view: (Have to spell it out) ") 
-    if credentials[choice] == None : ## ISSUE: The "null" situation just crashes the code, needs to be fixed.
+    choice = input("Which one would you like to view: (Have to spell it out) ")
+    ## Check if the choice is valid, if not return to main menu. If it is valid, print the credentials.
+    try:        credentials[choice]
+    except KeyError:
         loading()
         print("Sorry there are no credentials saved under this name")
-    else :
-        loading()
-        print(credentials[choice])
+        return 
+    loading()
+    print(credentials[choice])
 
-    pass_option = input("Would you like to change your password? (y to change, anything else to not)") ## Current issue, code only accepts y and should also accept n, anything else should prompt an error
+    pass_option = input("Would you like to change your password? (y to change, anything else to not)") ## Current issue, code only accepts y and should also accept n, anything else should prompt an error (is this really necessary? maybe just accept y and that anything else should be treated as n)
     if pass_option == "y" :
         new_pass = input("Set your new password: ")
         loading()
         credentials[choice]["password"] = new_pass
-        with open('data.json', 'w') as file:
-            json.dump(credentials, file, indent = 4)
+        ##Save the new password to the vault
+        save_vault(credentials, master_password)
         print("Done, password changed!")
     else:
         loading()
