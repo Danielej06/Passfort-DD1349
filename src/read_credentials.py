6d543d0e191b13aa0bceff48f915617a36746ab0
Daniel <daeo@kth.se>
2026-05-13 22:50:24 +0200

    Refactor: Update vault file handling with SQL and improve credential input/output logic

--- src/read_credentials.py
+++ src/read_credentials.py
@@ -1,37 +1,47 @@
 ## This file is used to read one credential from the vault and print to the console aswell as changing the password.
-from crypto.vault import load_vault, save_vault
+from crypto.vault import load_urls, load_vault, update_vault
 from loading import loading
 
 def read_credentials(master_password):
     ## Open the vault and read the contents to saved_credentials
-    if load_vault(master_password) == {} : ## ISSUE: The "null" situation just crashes the code, needs to be fixed.
+    urls = load_urls()
+    if urls == [] :
         print("Sorry there are no credentials saved yet. Returning to main menu.")
         return
-    credentials = load_vault(master_password)
 
     i = 1
     print("Here are your saved credentials:")
-    for cr_name in credentials:
+    for cr_name in urls:
         print(i, ". " , cr_name)
         i += 1
     
-    choice = input("Which one would you like to view: (Have to spell it out) ")
+    choice = input("Which number would you like to view: ")
+    while True:
+        try:
+            choice = int(choice)
+            if choice < 1 or choice > len(urls):
+                raise ValueError
+            break
+        except ValueError:
+            choice = input("Invalid choice, please enter a number between 1 and " + str(len(urls)) + ": ")
     ## Check if the choice is valid, if not return to main menu. If it is valid, print the credentials.
-    try:        credentials[choice]
+    
+    try:
+        credentials = load_vault(master_password, choice)
     except KeyError:
         loading()
         print("Sorry there are no credentials saved under this name")
         return 
     loading()
-    print(credentials[choice])
+    print(credentials)
 
-    pass_option = input("Would you like to change your password? (y to change, anything else to not)") ## Current issue, code only accepts y and should also accept n, anything else should prompt an error (is this really necessary? maybe just accept y and that anything else should be treated as n)
+    pass_option = input("Would you like to change your password? (y to change, anything else to not) ") ## Current issue, code only accepts y and should also accept n, anything else should prompt an error (is this really necessary? maybe just accept y and that anything else should be treated as n)
     if pass_option == "y" :
         new_pass = input("Set your new password: ")
         loading()
-        credentials[choice]["password"] = new_pass
+        credentials["password"] = new_pass
         ##Save the new password to the vault
-        save_vault(credentials, master_password)
+        update_vault(credentials, master_password, choice)
         print("Done, password changed!")
     else:
         loading()
