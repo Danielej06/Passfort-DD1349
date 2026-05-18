585d60677dea9e6bffd9eaf0a52a5d6080c27e70
Daniel <daeo@kth.se>
2026-05-18 15:49:00 +0200

    Refactor: Enhance session management to handle user inactivity and master password verification

--- src/session_manager.py
+++ src/session_manager.py
@@ -1,39 +1,44 @@
 # Följande fil skapas för att se till att en användare blir utloggad om de är inaktiva.
 # Detta är ett tillägg som är nödvändigt för att säkerställa säkerheten av programmet.
-
-
 import time
-
-
 # 2 minuter av inactivity innan man loggas ut
 LOGOUT_TIME = 120 
 
 # Återställer timern. Detta ska ske varje gång användaren interagerar. Denna används bara för första.
 USER_ACTIVE = time.time()
 
-# Återställer timern, men som funktion. Enklare att calla på.
-def reset_time() -> None:
-    global USER_ACTIVE # Så vi alltid arbetar med samma USER_ACTIVE
-    USER_ACTIVE = time.time()
+class SessionManager:
+    def __init__(self, started=False, logout_time=LOGOUT_TIME):
+        self.logout_time = logout_time
+        self.started = started
+        self.user_active = time.time()
+        self.key = None
 
-# Funktionen returnerar en bool beroende på tillståndet av USER_ACTIVE.
-# True om LOGOUT_TIME har gått. False om det fortfarande finns tid kvar.
-def session_state_expiry() -> bool:
-    inactivity = time.time() - USER_ACTIVE
-    compare = LOGOUT_TIME - inactivity
-    if compare > 0:
-        return False
-    elif compare <= 0:
-        return True
+    # Återställer timern, men som funktion. Enklare att calla på.
+    def reset_time(self):
+        self.user_active = time.time()
+        
+    def session_started(self):
+        self.started = True
+        self.reset_time()
 
-# Returnerar tiden innan du loggas ut. Bra att ha för något som "You will be logged out in 15 seconds".
-def time_left_before_logout() -> int:
-    inactivity = time.time() - USER_ACTIVE
-    remaining = LOGOUT_TIME - inactivity
+    # Funktionen returnerar en bool beroende på tillståndet av USER_ACTIVE.
+    # True om LOGOUT_TIME har gått. False om det fortfarande finns tid kvar.
+    def session_state_expiry(self) -> bool:
+        inactivity = time.time() - self.user_active
+        compare = self.logout_time - inactivity
+        if compare > 0:
+            return False
+        elif compare <= 0:
+            self.started = False
+            return True
 
-    # Använder max för att undvika negativa int on return.
-    return max(0, int(remaining))
-
+    # Returnerar tiden innan du loggas ut. Bra att ha för något som "You will be logged out in 15 seconds".
+    def time_left_before_logout(self):
+        inactivity = time.time() - self.user_active
+        remaining = self.logout_time - inactivity
+        # Använder max för att undvika negativa int on return. Om det är negativt, returnerar den 0 istället.
+        return max(0, int(remaining))
 
 
 
